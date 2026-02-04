9f1b91510f16e47bbbdbd8afa71c587b66113c84
V-Paritosh <paritoshv08@gmail.com>
2026-02-03 22:16:21 -0600

    Fixed Next build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["three"],
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 export default nextConfig;
